97cba3c2a8e4b5e6aa5ba68d8052e4ad6da2efcb
Drew Anello <drew.anello@gmail.com>
2025-01-14 13:15:37 -0500

    updates cors

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI
 from mangum import Mangum
 from app.endpoints import quotes, root
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET"],  # Allow only GET method
+    allow_headers=["*"],
+)
 handler = Mangum(app)
 
 app.include_router(root.router)
