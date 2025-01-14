de3076f71b827a95e8a6f9a0041e2d4ff56974d7
Drew Anello <drew.anello@gmail.com>
2025-01-14 13:16:01 -0500

    updates methods

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["GET"],  # Allow only GET method
+    allow_methods=["GET"],
     allow_headers=["*"],
 )
 handler = Mangum(app)
